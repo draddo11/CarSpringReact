ce92f5f46e71d0168771af90d04c49ee7499c843
Onesimus Addo Appiah <draddo11@gmail.com>
2024-01-14 19:28:12 +0000

    creating the entity

---
+++ src/main/java/com/nana/cardatabase/domain/Car.java
@@ -0,0 +1,15 @@
+package com.nana.cardatabase.domain;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+}
