596af6aa38bbe4175976f004dedff829d8e866e7
Onesimus Addo Appiah <draddo11@gmail.com>
2024-01-15 22:46:24 +0000

    Updated CardatabaseApplication.java with dummy data

--- src/main/java/com/nana/cardatabase/CardatabaseApplication.java
+++ src/main/java/com/nana/cardatabase/CardatabaseApplication.java
@@ -1,5 +1,6 @@
 package com.nana.cardatabase;
 
+import com.nana.cardatabase.domain.Car;
 import com.nana.cardatabase.domain.CarRepository;
 import org.slf4j.ILoggerFactory;
 import org.slf4j.Logger;
@@ -25,6 +26,16 @@ public class CardatabaseApplication implements CommandLineRunner {
 
     @Override
     public void run (String...args) throws Exception{
+        repository.save(new Car("Ford" , "Mustang"
+                ,"Red","ADF-1234" , 2024 ,56700));
 
+        repository.save(new Car("Toyota" , "Rav4"
+                ,"Ash","ADD-4554" , 2025 ,77700));
+
+        repository.save(new Car("Mecedes Benz" , "S 550"
+                ,"Black","ADF-5504" , 2026 ,80700));
+        for (Car car: repository.findAll()) {
+            logger.info(car.getBrand() + " " + car.getModel());
+        }
     }
 }
