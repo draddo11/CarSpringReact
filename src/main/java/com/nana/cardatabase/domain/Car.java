6b14c5f89fdd281848015cbaf6c845e2990be6ba
Onesimus Addo Appiah <draddo11@gmail.com>
2024-01-15 18:42:44 +0000

    DB created in H2 successfully

--- src/main/java/com/nana/cardatabase/domain/Car.java
+++ src/main/java/com/nana/cardatabase/domain/Car.java
@@ -1,10 +1,7 @@
 package com.nana.cardatabase.domain;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class Car {
@@ -13,7 +10,23 @@ public class Car {
     private long id;
     private String brand, model, color,
         registerNumber;
-    private int year, price;
+    @Column(name = "`year`")
+    private int year;
+    private int
+    price;
+
+    public Car(){}
+
+    public Car (String brand, String model,String color ,
+                String registerNumber, int year , int price){
+        super();
+        this.brand =brand;
+        this.model= model;
+        this.color=color;
+        this.registerNumber=registerNumber;
+        this.year=year;
+        this.price=price;
+    }
 
     public long getId() {
         return id;
