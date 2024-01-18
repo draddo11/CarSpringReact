ccc964352520539963c6abde4081f55468715bfb
Onesimus Addo Appiah <draddo11@gmail.com>
2024-01-18 22:45:18 +0000

    added reporestresource annotation to CarRepository

--- src/main/java/com/nana/cardatabase/domain/CarRepository.java
+++ src/main/java/com/nana/cardatabase/domain/CarRepository.java
@@ -1,7 +1,10 @@
 package com.nana.cardatabase.domain;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+
+@RepositoryRestResource
 public interface CarRepository extends
         CrudRepository<Car,Long> {
 }
