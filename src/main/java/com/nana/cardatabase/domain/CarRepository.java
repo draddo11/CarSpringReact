6a8aa97d4c617fd4db679770ec302f9839340e9b
Onesimus Addo Appiah <draddo11@gmail.com>
2024-01-19 20:44:38 +0000

    changed endpoint from cars to sort by color and brands of car

--- src/main/java/com/nana/cardatabase/domain/CarRepository.java
+++ src/main/java/com/nana/cardatabase/domain/CarRepository.java
@@ -1,10 +1,19 @@
 package com.nana.cardatabase.domain;
 
+//import org.hibernate.mapping.List;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
 
 
 @RepositoryRestResource(path="vehicles")
 public interface CarRepository extends
         CrudRepository<Car,Long> {
+    //fetch cars by brands
+    List<Car> findByBrand (@Param("brand")String brand);
+
+    //fetch cars by color
+    List<Car> findbyColor(@Param("color") String color);
 }
